f071103eca70647688f12816cc4210d11fe482af
toxx <txx1337@proton.me>
2024-12-24 01:16:45 +0100

    add simple get method, remove port 8080 from traefik expose

--- src/main/kotlin/de/bgs/primary/BoardGameController.kt
+++ src/main/kotlin/de/bgs/primary/BoardGameController.kt
@@ -3,14 +3,18 @@ package de.bgs.primary
 import de.bgs.core.BoardGameService
 import de.bgs.secondary.database.BoardGameItem
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
 
 @RestController
-@RequestMapping("/boardgame")
 class BoardGameController(private val boardGameService: BoardGameService) {
 
     @GetMapping
+    fun getHello(): String {
+        return "Hello. It is ${LocalDateTime.now()}"
+    }
+
+    @GetMapping("/boardgame")
     fun getBoardGame(): List<BoardGameItem> {
         return boardGameService.getBoardGameItems()
     }
