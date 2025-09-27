aabd9ef2d4597a0e408e5288628e49a0251a05ba
toxx <txx1337@proton.me>
2025-09-27 18:04:58 +0200

    use mutex to lock updates

--- src/main/kotlin/de/bgs/core/UpdateService.kt
+++ src/main/kotlin/de/bgs/core/UpdateService.kt
@@ -11,6 +11,8 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.stream.consumeAsFlow
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import org.eclipse.jgit.lib.Repository
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
@@ -39,6 +41,7 @@ class UpdateService(
     private var skipFirstExecution = gitConfigurationProperties.skipFirstExecution
     private val utcZone = ZoneOffset.UTC
     private val serviceScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+    private val mutex = Mutex()
 
     @Scheduled(timeUnit = DAYS, fixedRate = 7)
     private suspend fun scheduledUpdateDatabase() {
@@ -58,66 +61,73 @@ class UpdateService(
 
     @OptIn(ExperimentalCoroutinesApi::class)
     private suspend fun updateDatabase() = withContext(Dispatchers.IO) {
-        val updateStartTime: LocalDateTime = LocalDateTime.now(utcZone)
-        val lastExecution = updateTaskInformationRepo.findTopByOrderByLastUpdateTaskTimeDesc()?.lastUpdateTaskTime
-            ?: LocalDateTime.MIN
-        val atLeastOneDaySinceLastExecution = lastExecution.plusDays(1)
-            .isBefore(updateStartTime)
-        if (!atLeastOneDaySinceLastExecution) {
-            logger.info { "Skipping execution of update task. Last execution was less than 24h ago." }
+        if (!mutex.tryLock()) {
+            logger.info { "Update task is locked, skipping execution." }
             return@withContext
         }
-
-        boardGameService.clearDatabase()
-        logger.info { "Database has been cleared!" }
-
-        // update/pull git repo
-        val repo: Repository = getRepository()
-        gitService.pull(repo)
-
-        // parse CSVs and update database
-        val gameFamilyMap: Map<Long, GameFamily> =
-            gameFamilyJpaRepo.saveAll(csvService.parseGameFamily(repo.workTree)).associateBy { it.bggId }
-        logger.info { "Successfully saved ${gameFamilyMap.size} GameFamilies" }
-        val gameTypeMap = gameTypeJpaRepo.saveAll(csvService.parseGameType(repo.workTree)).associateBy { it.bggId }
-        logger.info { "Successfully saved ${gameTypeMap.size} GameTypes" }
-        val personMap = personJpaRepo.saveAll(csvService.parsePerson(repo.workTree)).associateBy { it.bggId }
-        logger.info { "Successfully saved ${personMap.size} Persons" }
-        val categoryMap = categoryJpaRepo.saveAll(csvService.parseCategory(repo.workTree)).associateBy { it.bggId }
-        logger.info { "Successfully saved ${categoryMap.size} Categories" }
-        val mechanicMap = mechanicJpaRepo.saveAll(csvService.parseMechanic(repo.workTree)).associateBy { it.bggId }
-        logger.info { "Successfully saved ${mechanicMap.size} Mechanics" }
-        val publisherMap = publisherJpaRepo.saveAll(csvService.parsePublisher(repo.workTree)).associateBy { it.bggId }
-        logger.info { "Successfully saved ${publisherMap.size} Publisher" }
-
-        var totalCounter = 0
-        csvService.parseBoardGamesStream(
-            repo.workTree,
-            gameFamilyMap,
-            gameTypeMap,
-            personMap,
-            categoryMap,
-            mechanicMap,
-            publisherMap
-        ).consumeAsFlow()
-            .buffer(capacity = 16, onBufferOverflow = BufferOverflow.SUSPEND)
-            .flowOn(Dispatchers.IO)
-            .chunked(500)
-            .onEach { items ->
-                boardGameJpaRepo.saveAll(items)
-                totalCounter += items.size
-                logger.info { "Executed Batch of size ${items.size}. # items saved: $totalCounter" }
-                System.gc()
+        mutex.withLock {
+            val updateStartTime: LocalDateTime = LocalDateTime.now(utcZone)
+            val lastExecution = updateTaskInformationRepo.findTopByOrderByLastUpdateTaskTimeDesc()?.lastUpdateTaskTime
+                ?: LocalDateTime.MIN
+            val atLeastOneDaySinceLastExecution = lastExecution.plusDays(1)
+                .isBefore(updateStartTime)
+            if (!atLeastOneDaySinceLastExecution) {
+                logger.info { "Skipping execution of update task. Last execution was less than 24h ago." }
+                return@withContext
             }
-            .catch { e -> logger.error(e) { "Pipeline failed $e" } }
-            .collect()
-        val duration = Duration.between(updateStartTime, LocalDateTime.now(utcZone))
-        val info = UpdateTaskInformation(
-            lastUpdateTaskTime = updateStartTime,
-            executionDurationInMinutes = duration.toMinutes()
-        )
-        updateTaskInformationRepo.save<UpdateTaskInformation>(info)
-        logger.info { "finished update-job successfully in ${duration.toHoursPart()}H:${duration.toMinutesPart()}M:${duration.toSecondsPart()}S!" }
+
+            boardGameService.clearDatabase()
+            logger.info { "Database has been cleared!" }
+
+            // update/pull git repo
+            val repo: Repository = getRepository()
+            gitService.pull(repo)
+
+            // parse CSVs and update database
+            val gameFamilyMap: Map<Long, GameFamily> =
+                gameFamilyJpaRepo.saveAll(csvService.parseGameFamily(repo.workTree)).associateBy { it.bggId }
+            logger.info { "Successfully saved ${gameFamilyMap.size} GameFamilies" }
+            val gameTypeMap = gameTypeJpaRepo.saveAll(csvService.parseGameType(repo.workTree)).associateBy { it.bggId }
+            logger.info { "Successfully saved ${gameTypeMap.size} GameTypes" }
+            val personMap = personJpaRepo.saveAll(csvService.parsePerson(repo.workTree)).associateBy { it.bggId }
+            logger.info { "Successfully saved ${personMap.size} Persons" }
+            val categoryMap = categoryJpaRepo.saveAll(csvService.parseCategory(repo.workTree)).associateBy { it.bggId }
+            logger.info { "Successfully saved ${categoryMap.size} Categories" }
+            val mechanicMap = mechanicJpaRepo.saveAll(csvService.parseMechanic(repo.workTree)).associateBy { it.bggId }
+            logger.info { "Successfully saved ${mechanicMap.size} Mechanics" }
+            val publisherMap =
+                publisherJpaRepo.saveAll(csvService.parsePublisher(repo.workTree)).associateBy { it.bggId }
+            logger.info { "Successfully saved ${publisherMap.size} Publisher" }
+
+            var totalCounter = 0
+            csvService.parseBoardGamesStream(
+                repo.workTree,
+                gameFamilyMap,
+                gameTypeMap,
+                personMap,
+                categoryMap,
+                mechanicMap,
+                publisherMap
+            ).consumeAsFlow()
+                .buffer(capacity = 16, onBufferOverflow = BufferOverflow.SUSPEND)
+                .flowOn(Dispatchers.IO)
+                .chunked(500)
+                .onEach { items ->
+                    boardGameJpaRepo.saveAll(items)
+                    totalCounter += items.size
+                    logger.info { "Executed Batch of size ${items.size}. # items saved: $totalCounter" }
+                    System.gc()
+                }
+                .catch { e -> logger.error(e) { "Pipeline failed $e" } }
+                .collect()
+            val duration = Duration.between(updateStartTime, LocalDateTime.now(utcZone))
+            val info = UpdateTaskInformation(
+                lastUpdateTaskTime = updateStartTime,
+                executionDurationInMinutes = duration.toMinutes()
+            )
+            updateTaskInformationRepo.save<UpdateTaskInformation>(info)
+            logger.info { "finished update-job successfully in ${duration.toHoursPart()}H:${duration.toMinutesPart()}M:${duration.toSecondsPart()}S!" }
+        }
     }
 
     fun getRepository(): Repository {
