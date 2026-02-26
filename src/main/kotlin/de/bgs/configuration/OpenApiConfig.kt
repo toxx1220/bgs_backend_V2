38bd6a98601c107096bd1db5d21f808b3e183aed
toxx <txx1337@proton.me>
2026-02-26 13:36:23 +0100

    add impressum and data privacy to swagger api

--- src/main/kotlin/de/bgs/configuration/OpenApiConfig.kt
+++ src/main/kotlin/de/bgs/configuration/OpenApiConfig.kt
@@ -1,6 +1,9 @@
 package de.bgs.configuration
 
+import io.swagger.v3.oas.models.ExternalDocumentation
 import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Contact
+import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -18,6 +21,19 @@ class OpenApiConfig() {
             .addServersItem(
                 Server()
                     .url("http://localhost:8080")
+            )
+            .externalDocs(
+                ExternalDocumentation()
+                    .description("Datenschutz")
+                    .url("https://toxx.dev/datenschutz"),
+            )
+            .info(
+                Info()
+                    .contact(
+                        Contact()
+                            .name("Impressum")
+                            .url("https://toxx.dev/impressum")
+                    )
             )
     }
 }
